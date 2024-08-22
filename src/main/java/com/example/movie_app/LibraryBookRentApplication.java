d1376b56c446d8ef6a501d980a4a56c0dc3a1c14
ty_sumeet <sumeet.k@testyantra.in>
2024-08-22 11:23:39 +0530

    added package structure

---
+++ src/main/java/com/example/movie_app/LibraryBookRentApplication.java
@@ -0,0 +1,13 @@
+package com.example.movie_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBookRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryBookRentApplication.class, args);
+	}
+
+}
